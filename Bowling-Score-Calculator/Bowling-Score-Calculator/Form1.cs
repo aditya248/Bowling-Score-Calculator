473a9e6e313c62bb8ae5c7741991595f6d249c3f
Unknown <adityasinha248@gmail.com>
2019-10-02 19:11:14 -0500

    Added strike class and global variables.

--- Bowling-Score-Calculator/Bowling-Score-Calculator/Form1.cs
+++ Bowling-Score-Calculator/Bowling-Score-Calculator/Form1.cs
@@ -12,9 +12,25 @@ namespace Bowling_Score_Calculator
 {
     public partial class Form1 : Form
     {
+        #region Global Variables
+
+        int score = 0; //Keeps track of score.
+        int frame = 1; //Keeps track of frame.
+        int throws = 20; //No. of throws in a game of bowling.
+        bool spare = false; // True if spare.
+        int spareCount = 0; // Store the extra point for spare.
+        bool extraRound = true;// Allows to give 2 or 1 extra throws depending on strike or spare.
+        bool isStrike = false; // True if strike
+        List<Strike> strike = new List<Strike>(); // Stores extra points for strike
+
+        #endregion Global Variables
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+
     }
 }
---
+++ Bowling-Score-Calculator/Bowling-Score-Calculator/Strike.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling_Score_Calculator
+{
+    /// <summary>
+    /// Strike class to store extra strike points.
+    /// </summary>
+    class Strike
+    {
+        public int point1;
+        public int point2;
+        public Strike()
+        {
+            //Initilaized as negative number as 0 to 10 are possible values.
+            point1 = -1;
+            point2 = -1;
+        }
+    }
+}
